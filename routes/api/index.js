9a6a90ab35b8dc74d941ca990bab95530b9dc653
mflanigan <mflanigantwualumn@gmail.com>
2022-03-15 15:07:17 -0500

    working on APIs

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,8 @@
 const router = require('express').Router();
-// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-// add prefix of `/api` to all of the api routes imported from the `api` directory
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
